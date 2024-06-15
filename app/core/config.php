936cbe4a756d271d60e35e7a8b67f7f2c410c8a4
ShivanshuKaushal <shivanshu02@algomau.ca>
2024-06-15 02:32:55 +0000

    Connected to Database

--- app/core/config.php
+++ app/core/config.php
@@ -14,8 +14,8 @@ define('LOGS', ROOT . DS . 'logs');
 define('FILES', ROOT . DS. 'files');
 
 // ---------------------  NEW DATABASE TABLE -------------------------
-define('DB_HOST',         'xxw.h.filess.io');
-define('DB_USER',         '4806_sentenceby'); 
+define('DB_HOST',         'm43.h.filess.io');
+define('DB_USER',         'Cosc4806_rhythmkept'); 
 define('DB_PASS',         $_ENV['DB_PASS']);
-define('DB_DATABASE',     '4806_sentenceby');
+define('DB_DATABASE',     'Cosc4806_rhythmkept');
 define('DB_PORT',         '3305');
